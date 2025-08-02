015e2fad25b077f241aef3f2dd46f76d644381cd
Willian Amorim <willianamorim1@outlook.com.br>
2025-08-02 12:28:25 -0300

    configurado e testado meu arquivo server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,14 @@
-console.log('Server is running on port 3333');
+import express, {json, Request, Response} from 'express';
+
+const app = express();
+const port = process.env.PORT
+
+app.use(json())
+
+app.get('/', (req: Request, res: Response) => {
+    res.status(200).json({ player: 'Willian' });
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+})
